009a488334c4c8929632113c3028a002dab2482a
Robinson_Xavier <robinson.webzen@gmail.com>
2022-09-11 21:39:40 -0300

    added sign-up post

--- src/index.js
+++ src/index.js
@@ -2,22 +2,73 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
-
-dotenv.config();
+import joi from 'joi';
+import bcrypt from 'bcrypt';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+
+//SCHEMAS PARA VALIDAR
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().email().required()
+});
+
+//PATH CRIADO PARA SER CAPAZ DE LER O ARQUIVO .ENV NA PASTA PRINCIPAL
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env')
+  })
+
+//CONEXAO ESTABELECIDA NO MONGO AO DB MYWALLET
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-
 let db;
-
 mongoClient.connect().then(() => {
     db = mongoClient.db('mywallet');
 });
 
+//ATIVIDADES DISPONIVEIS NO SERVIDOR
 
-app.use(express.json());
-app.use(cors());
+app.post('/sign-up', async (req, res) => {
+
+    const {name, email, password, confirmPassword} = req.body;
+
+    const validation = userSchema.validate(user, {abortEarly: false});
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        res.status(422).send(errors);
+        return;
+    }
+
+    if ( confirmPassword !== password) {
+        res.status(422).send("O campo de confirmação deve ser preenchido corretamente com a mesma senha que o campo acima.");
+        return;
+    }
+
+    const hashPassword = bcrypt.hashSync(password, 10);
+
+    try {
+        await db.collection('users').insertOne({
+            name,
+            email,
+            password: hashPassword
+        });
+        res.sendStatus(201);
+    } catch (error) {
+        console.error(error); 
+        return res.sendStatus(500);
+    }
+
+
+});
 
 
 
